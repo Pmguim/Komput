55c4ee26a305eaa4e0368f4778e9864b8e9bd3c2
Arthur Souza Sepp <arthursouzasepp@gmail.com>
2020-12-01 01:45:43 -0300

    Realizando experimentos com python e atualizando as anotações.

---
+++ Python/Arquivos/Scripts_Python/Curso_Python/Exercícios/ex_005.py
@@ -0,0 +1,6 @@
+print('Olá humano!')
+
+Valor1 = int(input('Insira um valor: '))
+Ant = Valor1 - 1
+Suc = Valor1 + 1
+print('Baseado no valor {0}, o antecessor é {1} e o sucessor é {2}'.format(Valor1, Ant, Suc))
---
+++ Python/Arquivos/Scripts_Python/Curso_Python/Exercícios/ex_006.py
@@ -0,0 +1,6 @@
+print('Olá humano!')
+
+Valor1 = int(input('Insira um valor: '))
+
+print('Baseado no valor {}, o dobro dele é {}, o triplo {}, e a sua raíz é de {:.2f}.'
+.format(Valor1, (Valor1*2), (Valor1*3), (Valor1**(1/2))))
